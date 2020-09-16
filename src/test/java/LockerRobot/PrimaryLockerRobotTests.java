b65ce5cb8a974e9a98f865e648c0739aeeaa9665
Dong Biao <biao.dong@thoughtworks.com>
2020-09-16 20:21:40 +0800

    Given M bag and all lockers no capacity, should return error

--- src/test/java/LockerRobot/PrimaryLockerRobotTests.java
+++ src/test/java/LockerRobot/PrimaryLockerRobotTests.java
@@ -1,10 +1,10 @@
 package LockerRobot;
 
+import LockerRobot.Exceptions.NoCapacityException;
 import com.google.common.collect.ImmutableList;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PrimaryLockerRobotTests {
     private Types lockerType = Types.M;
@@ -37,5 +37,16 @@ public class PrimaryLockerRobotTests {
         assertNotNull(actual);
         assertEquals(Types.M, actual.getTicketType());
         assertEquals(bag, secondLocker.pickUpBag(actual));
+    }
+
+    @Test
+    void should_not_save_bag_and_return_error_when_save_bag_given_a_M_bag_and_lockers_has_no_capacity() {
+        Bag bag = new Bag(Types.M);
+        Locker firstLocker = new Locker(0, lockerType);
+        Locker secondLocker = new Locker(0, lockerType);
+
+        PrimaryLockerRobot primaryLockerRobot = new PrimaryLockerRobot(ImmutableList.of(firstLocker, secondLocker));
+
+        assertThrows(NoCapacityException.class, () -> primaryLockerRobot.saveBag(bag));
     }
 }
