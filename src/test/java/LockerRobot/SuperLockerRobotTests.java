91537e4524661bc6e013aec6e7fc79b2d5e9e4a2
Dong Biao <biao.dong@thoughtworks.com>
2020-09-17 20:52:44 +0800

    Given same free capacity lockers, should save into 1st locker for SuperLockerRobot

--- src/test/java/LockerRobot/SuperLockerRobotTests.java
+++ src/test/java/LockerRobot/SuperLockerRobotTests.java
@@ -40,4 +40,19 @@ public class SuperLockerRobotTests {
         assertEquals(Types.L, actual.getTicketType());
         assertEquals(bag, secondLocker.pickUpBag(actual));
     }
+
+    @Test
+    void should_save_bag_in_1st_locker_and_return_L_ticket_when_save_bag_given_a_L_bag_and_all_lockers_has_same_free_capacity() {
+        Bag bag = new Bag(Types.L);
+        Locker firstLocker = new Locker(2, lockerType);
+        Locker secondLocker = new Locker(2, lockerType);
+
+        SuperLockerRobot superLockerRobot = new SuperLockerRobot(ImmutableList.of(firstLocker, secondLocker));
+
+        Ticket actual = superLockerRobot.saveBag(bag);
+
+        assertNotNull(actual);
+        assertEquals(Types.L, actual.getTicketType());
+        assertEquals(bag, firstLocker.pickUpBag(actual));
+    }
 }
