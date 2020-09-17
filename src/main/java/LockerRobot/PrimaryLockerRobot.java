30bd217ef23b7869124fef249cf66f004eaadc96
Dong Biao <biao.dong@thoughtworks.com>
2020-09-17 21:17:11 +0800

    Given S ticket and PrimaryLockerRobot, should return error

--- src/main/java/LockerRobot/PrimaryLockerRobot.java
+++ src/main/java/LockerRobot/PrimaryLockerRobot.java
@@ -2,11 +2,13 @@ package LockerRobot;
 
 import LockerRobot.Exceptions.FakeTicketException;
 import LockerRobot.Exceptions.NoCapacityException;
+import LockerRobot.Exceptions.TypeNotMatchedException;
 
 import java.util.List;
 
 public class PrimaryLockerRobot {
     private List<Locker> lockers;
+    private Types robotType = Types.M;
 
     public PrimaryLockerRobot(List<Locker> lockers) {
         this.lockers = lockers;
@@ -18,6 +20,10 @@ public class PrimaryLockerRobot {
     }
 
     public Bag pickUpBag(Ticket ticket) {
+        if(ticket.getTicketType() != this.robotType) {
+            throw new TypeNotMatchedException(this.getClass().getName());
+        }
+
         Locker locker = lockers.stream().filter(l -> l.isTicketContained(ticket)).findFirst().orElseThrow(() -> new FakeTicketException(this.getClass().getName()));
         return locker.pickUpBag(ticket);
     }
