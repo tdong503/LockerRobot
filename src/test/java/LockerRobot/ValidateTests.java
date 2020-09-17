b57107a0b10680bd4a8a85c7e955985538869f7e
Dong Biao <biao.dong@thoughtworks.com>
2020-09-17 21:12:43 +0800

    Given L ticket and S locker, should return error

--- src/test/java/LockerRobot/ValidateTests.java
+++ src/test/java/LockerRobot/ValidateTests.java
@@ -17,4 +17,15 @@ public class ValidateTests {
 
         assertThrows(TypeNotMatchedException.class, () -> locker.pickUpBag(ticket));
     }
+
+    @Test
+    void should_not_get_bag_and_throw_error_when_locker_get_bag_given_a_L_ticket_generated_by_SuperLockerRobot() {
+        Bag bag = new Bag(Types.L);
+        PrimaryLockerRobot primaryLockerRobot = new PrimaryLockerRobot(ImmutableList.of(new Locker(1, Types.L)));
+        Ticket ticket = primaryLockerRobot.saveBag(bag);
+
+        Locker locker = new Locker(1, Types.S);
+
+        assertThrows(TypeNotMatchedException.class, () -> locker.pickUpBag(ticket));
+    }
 }
