6b5586a5470028e070c93e5b9bb2be259d7e456a
Dong Biao <dong.biao@myob.com>
2020-09-18 17:56:21 +0800

    Given a SuperLockerRobot and setup a S locker, should get error

--- src/test/java/lockerrobot/ValidateTests.java
+++ src/test/java/lockerrobot/ValidateTests.java
@@ -111,4 +111,13 @@ class ValidateTests {
 
         assertThrows(SetupNotMatchedException.class, () -> new PrimaryLockerRobot(lockers));
     }
+
+    @Test
+    void should_throw_error_when_setup_SuperLockerRobot_given_a_S_locker() {
+        Locker locker = new Locker(1, Types.S);
+
+        List<Locker> lockers = ImmutableList.of(locker);
+
+        assertThrows(SetupNotMatchedException.class, () -> new SuperLockerRobot(lockers));
+    }
 }
